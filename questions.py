0a50ea6d6d5831e2dfd5b7a71c516579f350b14e
csPython <cspython@AD25TF04NGF1J-33.local>
2019-01-09 12:42:41 -0500

    initialize file

---
+++ questions.py
@@ -0,0 +1,9 @@
+q1 = False
+answer = int (0)
+
+print("Hello, and welcome to my extremely complicated quiz.")
+print("In this program, you will have to answer one question.")
+print("Hit enter to start.")
+input("")
+
+
